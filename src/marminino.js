7112a3c543f788cb6c7a78a156315483e4ea22e7
Karla Danielle <daniellenogueira30@gmail.com>
2024-03-02 15:50:16 -0300

    teste prova

--- src/marminino.js
+++ src/marminino.js
@@ -1,5 +1,21 @@
 function executar(numero) {
-    // yourcode
+   let palavra = '';
+   
+   if(numero % 3 === 0) {
+    palavra += 'Mar';
+    }
+
+   if(numero % 4 === 0) {
+    palavra += 'Minino';
+    }
+
+    return palavra;
+
+}   
+
+for(let i = 1; i <= 100; i++) {
+    let resultado = executar (i);
+    console.log({i, resultado})
 }
 
 module.exports = executar;
